38da30ded573a7e6f9049efa31c615f94a93e725
JuanHRST <juanherreras1895@gmail.com>
2023-02-20 20:39:37 -0600

    codigo otros 5

--- Codigo5.java
+++ Codigo5.java
@@ -1,29 +1,31 @@
+import java.util.Scanner; //Se agrego el Scanner
+//Contar si tiene mas (3, 7, 8, 9) que los numeros (1, 2, 4, 5, 6, 0)
 public class Codigo5 {
+	public static void main(String[] args) { //Se agregó el public static void main
+		 Scanner s = new Scanner(System.in); // Se agrego System.in
 
-	    Scanner s = new Scanner();
-	    System.out.print("Introduzca un número: ');
-	    String ni = s.nextLine();
-	    int c = ni;
-	    
-	    int afo = 0;
-	    int noAfo = 0;
-	    
-	    while (ni > 0) {
-		  int digito = (int)(ni % 10);
-	      if ((digito == 3) || (digito == 7) || (digito == 8) || (digito == 9)) {
-			afo++;
-	      } else {
-			noAfo++;
-		  
-		  ni /= 10;
-	    }
+		System.out.print("Introduzca un número: ");
+		int ni = s.nextInt(); //Se cambio el tipo de dato de String a Integer
+		int c = ni; 
+		
+		int afo = 0;
+		int noAfo = 0;
 
-	    if (afo > noAfo) {
-	      System.out.prinln("El " + c + " es un número afortunado.");
-	    } else {
-	      System.out.println("El " + c + " no es un número afortunado.");
-	    }
-	    
-	  }
-	  
+		while (ni > 0) {
+			int digito = (int) (ni % 10);
+			if ((digito == 3) || (digito == 7) || (digito == 8) || (digito == 9)) {
+				afo++;
+			} else {
+				noAfo++;
+			}
+			ni /= 10; //Se movió esta linea fuera del else
+			}
+
+			if (afo > noAfo) {
+				System.out.println("El " + c + " es un número afortunado."); //se agrego "t" en el syso
+			} else {
+				System.out.println("El " + c + " no es un número afortunado.");
+			}
+
 	}
+}
